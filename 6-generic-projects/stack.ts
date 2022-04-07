7414ffa23ec60812b52f74085cd754d129020acf
crud0626 <sisofiy6048@gmail.com>
2022-04-07 11:41:02 +0900

    Add generics quiz and complete chapter 7

---
+++ 6-generic-projects/stack.ts
@@ -0,0 +1,65 @@
+{
+    interface Stack<T> {
+        readonly size: number;
+        push(value: T): void;
+        pop(): T;
+    }
+
+    type StackNode<T> = {
+        readonly value: T;
+        readonly next?: StackNode<T>;
+    }
+
+    class StackImpl<T> implements Stack<T> {
+        private _size: number = 0;
+        private head?: StackNode<T>;
+
+        constructor(private capacity?: number) {}
+
+        get size() {
+            return this._size;
+        }
+
+        push(value: T): void {
+            if(this.size === this.capacity) {
+                throw new Error("Stack is full!");
+            }
+
+            // const node: StackNode<T> = {
+            const node = { // 위의 타입에서 정의했기 때문에 생략해도 괜찮다.
+                value,
+                next: this.head
+            }
+            this.head = node;
+            this._size++;
+        }
+
+        pop(): T {
+            if(this.head == null) {
+                throw new Error('Stack is empty!');
+            }
+            
+            const node = this.head;
+            this.head = node.next;
+            this._size--; 
+            return node.value;
+        }
+    }
+
+    const stack = new StackImpl<string>(10); // 타입 지정안하면 unknown이 됨.
+    stack.push("Ellie 1");
+    stack.push("Bob 2");
+    stack.push("Steve 3");
+    while(stack.size !== 0) {
+        console.log(stack.pop());
+    }
+
+    const stack2 = new StackImpl<number>(10); // 타입 지정안하면 unknown이 됨.
+    stack2.push(1);
+    stack2.push(2);
+    stack2.push(3);
+    while(stack2.size !== 0) {
+        console.log(stack2.pop());
+    }
+
+}
