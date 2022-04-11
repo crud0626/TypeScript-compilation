0d07a05263e5c315c9e31a23e0e52a07efaac63e
crud0626 <sisofiy6048@gmail.com>
2022-04-11 16:23:50 +0900

    Add index.ts until 10.5

---
+++ 9-advanced/9-2-index.ts
@@ -0,0 +1,31 @@
+{
+    // Utility types
+    const obj = {
+        name: 'ellie';
+    }
+
+    type Animal = {
+        name: string;
+        age: number;
+        gender: 'male' | 'female';
+    };
+
+    type Name = Animal['name']; // Animal 타입에서 name의 타입인 string만 빼 올 수 있음.
+    const text: Name = "hello";
+
+    type Gender = Animal['gender'];
+
+    // Animal의 모든 키 타입을 Union type으로 할당 (name, age, gender)
+    type Keys = keyof Animal;
+    const key: Keys = 'age';
+
+    type Person = {
+        name: string;
+        gender: Animal['gender'];
+    }
+
+    const person: Person = {
+        name: 'ellie',
+        gender: 'female'
+    }
+}
